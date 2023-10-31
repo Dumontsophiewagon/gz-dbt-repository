ba52fcae9d98f2a5d43085d9cc58463867c5fad9
sophie.dumont.lewagon@gmail.com <sophie.dumont.lewagon@gmail.com>
2023-10-31 14:06:46 +0000

    operational margin by day and kpi

---
+++ models/mart/finance_days.sql
@@ -0,0 +1,9 @@
+SELECT
+date_date
+,COUNT(orders_id) as nb_transactions
+,ROUND(SUM(revenue),2) as revenue
+,ROUND(SUM(revenue)/COUNT(orders_id),2) as avg_basket
+,ROUND(SUM(margin),2) as margin
+,ROUND(SUM(operational_margin),2) as operational_margin
+FROM {{ref("int_orders_operational")}}
+GROUP BY date_date
